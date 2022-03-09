bed371b24a4526f1dff7b1c2c38017a95e901304
Saksham <ultron0603@gmail.com>
2022-03-10 01:18:45 +0530

    Pid controlled LFR

--- line_follower.ino
+++ line_follower.ino
@@ -3,29 +3,51 @@ bool a_digital[5];// 1 value for white surface and 0 for black surface
 //-ve for left +ve for right
 //motor_left1=3
 int error=0;
-int ki=0;
-int kp=0;
-int kd=0;
+float ki=0;
+float kp=17;
+float kd=2.8;
 
 int previous_error=0;
-const int thres=100;
-int speed_left=153;
-int speed_right=153;
+const int thres=20;
+int speed_left=50;
+int speed_right=50;
 int pid_value=0;
 int i=0;
 int d=0;
 
 void setup(){
-
+  Serial.begin(9600);
+  pinMode(3,OUTPUT);
+  
+pinMode(4,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(A0,INPUT);
+pinMode(A1,INPUT);
+pinMode(A2,INPUT);
+pinMode(A3,INPUT);
+pinMode(A4,INPUT);
 }
 
 void loop(){
-a[0]=analogRead();
-a[1]=analogRead();
-a[2]=analogRead();
-a[3]=analogRead();
-a[4]=analogRead();
-
+a[0]=analogRead(A0);
+a[1]=analogRead(A1);
+a[2]=analogRead(A2);
+a[3]=analogRead(A3);
+a[4]=analogRead(A4);
+Serial.print(a[0]);
+Serial.print(" ");
+Serial.print(a[1]);
+Serial.print(" ");
+Serial.print(a[2]);
+Serial.print(" ");
+Serial.print(a[3]);
+Serial.print(" ");
+Serial.print(a[4]);
+Serial.print(" ");
+Serial.print('\n');
 if(a[0]>thres){
   a_digital[0]=1;  
  }else a_digital[0]=0;
@@ -70,13 +92,15 @@ if(a[0]>thres){
   
   pid_value=kp*error+ki*i + kd*d;
 
-  speed_left=153-pid_value;
-  speed_right=153+pid_value;
+  speed_left=100-pid_value;
+  speed_right=100+pid_value;
 
   analogWrite(3,speed_left);
-  analogWrite(6,0);
+  digitalWrite(4,1);
   analogWrite(9,speed_right);
-  analogWrite(10,0);
+  digitalWrite(7,0);
+  digitalWrite(8,1);
+  digitalWrite(12,0);
   
   
  
