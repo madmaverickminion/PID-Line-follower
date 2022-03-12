65a052ba205fb3708ccfc9872ecbd06055dbd42e
Saksham <ultron0603@gmail.com>
2022-03-12 17:59:03 +0530

    PID- lfr final code

--- line_follower.ino
+++ line_follower.ino
@@ -1,14 +1,12 @@
 int a[5];
 bool a_digital[5];// 1 value for white surface and 0 for black surface 
-//-ve for left +ve for right
-//motor_left1=3
 int error=0;
 float ki=0;
-float kp=17;
+float kp=30;
 float kd=2.8;
 
 int previous_error=0;
-const int thres=20;
+const int thres=50;
 int speed_left=50;
 int speed_right=50;
 int pid_value=0;
@@ -37,17 +35,7 @@ a[1]=analogRead(A1);
 a[2]=analogRead(A2);
 a[3]=analogRead(A3);
 a[4]=analogRead(A4);
-Serial.print(a[0]);
-Serial.print(" ");
-Serial.print(a[1]);
-Serial.print(" ");
-Serial.print(a[2]);
-Serial.print(" ");
-Serial.print(a[3]);
-Serial.print(" ");
-Serial.print(a[4]);
-Serial.print(" ");
-Serial.print('\n');
+
 if(a[0]>thres){
   a_digital[0]=1;  
  }else a_digital[0]=0;
@@ -63,7 +51,7 @@ if(a[0]>thres){
  if(a[4]>thres){
   a_digital[4]=1;  
  }else a_digital[4]=0;
-
+ 
  if(a_digital[0]==1 && a_digital[1]==1 && a_digital[2]==0 && a_digital[3]==1 && a_digital[4]==1){
   error=0;
  }else if(a_digital[0]==1 && a_digital[1]==1 && a_digital[2]==0 && a_digital[3]==0 && a_digital[4]==1){
@@ -78,20 +66,31 @@ if(a[0]>thres){
  }else if(a_digital[0]==1 && a_digital[1]==0 && a_digital[2]==0 && a_digital[3]==1 && a_digital[4]==1){
   error=1;
  }else if(a_digital[0]==1 && a_digital[1]==0 && a_digital[2]==1 && a_digital[3]==1 && a_digital[4]==1){
-  //10111
   error=2;
  }else if(a_digital[0]==0 && a_digital[1]==0 && a_digital[2]==1 && a_digital[3]==1 && a_digital[4]==1){
   error=3;
  }else if(a_digital[0]==0 && a_digital[1]==1 && a_digital[2]==1 && a_digital[3]==1 && a_digital[4]==1){
   error=4;
  }
+ Serial.print(a[0]);
+ Serial.print(" " );
+ Serial.print(a[1]);
+ Serial.print(" " );
+ Serial.print(a[2] );
+ Serial.print(" " );
+ Serial.print(a[3]);
+ Serial.print(" " );
+ Serial.print(a[4]);
+ 
+ Serial.print("\n");
+ 
   i=i+previous_error;
   d=error-previous_error;
   
   previous_error=error;
   
   pid_value=kp*error+ki*i + kd*d;
-
+    
   speed_left=100-pid_value;
   speed_right=100+pid_value;
 
